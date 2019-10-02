923d5e9acf23c4a06a6213673298e5954b537aa0
wesley <wesleyyw8@gmail.com>
2019-10-02 21:39:00 +0100

    login state done

--- APM-Demo0/src/app/user/login.component.ts
+++ APM-Demo0/src/app/user/login.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { AuthService } from './auth.service';
+import { Store, select } from '@ngrx/store';
 
 @Component({
   templateUrl: './login.component.html',
@@ -15,11 +16,16 @@ export class LoginComponent implements OnInit {
   maskUserName: boolean;
 
   constructor(private authService: AuthService,
-              private router: Router) {
+              private router: Router,
+              private store: Store<any>) {
   }
 
   ngOnInit(): void {
-
+    this.store.pipe(select('user')).subscribe(user => {
+      if (user) {
+        this.maskUserName = user.maskUserName;
+      }
+    })
   }
 
   cancel(): void {
@@ -27,7 +33,10 @@ export class LoginComponent implements OnInit {
   }
 
   checkChanged(value: boolean): void {
-    this.maskUserName = value;
+    this.store.dispatch({
+      type: 'MASK_USER_NAME',
+      payload: value
+    });
   }
 
   login(loginForm: NgForm): void {
